c29f8011c047246ed9e02776b13d3ff8ce90b3dc
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-07-11 20:12:22 -0300

    Criando UsuariosController

--- WebMvcMySQL/WebMvcMySQL/Program.cs
+++ WebMvcMySQL/WebMvcMySQL/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WebMvcMySQL.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseMySql(
+        "server=localhost;initial catalog=CRUD_MVC_MYSQL_AULA; uid=root;pwd=1234567",
+         Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql")));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
